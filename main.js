e216073a5bf1523ad3cd475a5446497c926c5002
Matt Hensley <matt.hensley777@gmail.com>
2017-09-05 19:19:20 -0500

    step1

--- main.js
+++ main.js
@@ -1,1 +1,41 @@
-console.log("hello");
+// function moreCrap () {
+
+// }
+
+// let Something = (function(oldSomething){
+// 	const animals = ["cat", "dog"];
+// 	oldSomething.niceFunction = function () {
+// 		console.log("im a nice function")
+// 	}
+// 	return oldSomething;
+
+// })(Something || {});
+
+var SolarSystem = (function(oldSolarSystem){
+	const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
+	const spaceCraft = ["challenger", "enterprise", "death star"];
+	
+	oldSolarSystem.getPlanets = function () {
+		return planets;
+	}
+
+	oldSolarSystem.setPlanets = function (newPlanet){
+		planets.push(newPlanet);
+	}
+
+	oldSolarSystem.getSpaceCraft = function () {
+		return spaceCraft;
+	}
+
+	oldSolarSystem.setSpaceCraft = function (newSpaceCraft) {
+		spaceCraft.push(newSpaceCraft)
+	}
+
+	return oldSolarSystem
+})(SolarSystem || {});
+
+let myPlanets = SolarSystem.getPlanets();
+	console.log("myPlanets", myPlanets);
+	
+SolarSystem.getSpaceCraft();
+
